3bf69bddb89fe968eed6b9647ef1401b4aa6dcc7
David Barry <david.barry@ipg-automotive.com>
2022-02-19 23:16:15 +0000

    initial prototype

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# erg-file-importer
-JavaScript file importer for IPG Automotive .erg result files
+# IPG Automotive erg file reader
+
+This is a JavaScript file importer for IPG Automotive .erg result files. The importer requires a .erg file and corresponding .info file.
+
+The data is returned as an array of quantity objects. Each object represents a quantity and contains the following properties:
+
+- name: the name of the quantity
+- type: the data type of the quantity
+- unit: the unit of the quantity
+- values: array of values for the quantity
+
+Further information on the erg file format can be found in appendix A.1 of the IPG Automotive reference manual.
+
+## Install
+
+```
+npm install --save @ipguk/erg-file-reader
+```
+
+## Usage
+
+Import library
+
+```
+const erg = require("@ipguk/erg-file-reader");
+```
+
+Define the .erg and .info file paths
+
+```
+let ergFile = "MyFile.erg";
+let infoFile = "MyFile.erg.info";
+```
+
+Read the data
+
+```
+let data = erg.read(ergFile, infoFile);
+```
---
+++ index.js
@@ -0,0 +1,172 @@
+const fs = require("fs");
+
+// read erg and info file
+exports.read = function (ergFile, infoFile) {
+  // read info file
+  const info = fs.readFileSync(infoFile, "utf8").replace(/\r/g, "").split("\n");
+
+  // extract quantity info
+  let quantNumber = 1;
+  let lastQuantName = "";
+  let quants = [];
+  info.forEach((line) => {
+    // name
+    if (line.startsWith(`File.At.${quantNumber}.Name`)) {
+      lastQuantName = line.split("= ")[1];
+      quants.push({ name: lastQuantName, type: "", unit: "", values: [] });
+      quantNumber++;
+      return;
+    }
+
+    // type
+    if (line.startsWith(`File.At.${quantNumber - 1}.Type`)) {
+      quants[quants.length - 1].type = line.split("= ")[1];
+      return;
+    }
+
+    // unit
+    if (line.startsWith(`Quantity.${lastQuantName}.Unit`)) {
+      quants[quants.length - 1].unit = line.split("= ")[1];
+      return;
+    }
+  });
+
+  // read erg file
+  let ergBuffer = fs.readFileSync(ergFile);
+
+  // process erg header
+  let headerBuffer = ergBuffer.slice(0, 15);
+  let ergHeader = {
+    format: headerBuffer.toString("ascii", 0, 7),
+    version: headerBuffer.readUInt8(8),
+    byteOrder: headerBuffer.readUInt8(9),
+    recordSize: headerBuffer.readUInt8(10, 11),
+    reserved: headerBuffer.readUInt8(12, 15),
+  };
+
+  // process erg records by reading value for each quantity
+  let recordBuffer = ergBuffer.slice(16);
+  let recordIndex = 0;
+  while (recordIndex < recordBuffer.length) {
+    quants.forEach((quant) => {
+      switch (quant.type) {
+        case "Double": {
+          let value =
+            ergHeader.byteOrder == 0
+              ? recordBuffer.readDoubleLE(recordIndex)
+              : recordBuffer.readDoubleBE(recordIndex);
+          quant.values.push(value);
+          recordIndex += 8;
+          break;
+        }
+        case "Float": {
+          let value =
+            ergHeader.byteOrder == 0
+              ? recordBuffer.readFloatLE(recordIndex)
+              : recordBuffer.readFloatBE(recordIndex);
+          quant.values.push(value);
+          recordIndex += 4;
+          break;
+        }
+        case "LongLong": {
+          let value =
+            ergHeader.byteOrder == 0
+              ? recordBuffer.readIntLE(recordIndex, 8)
+              : recordBuffer.readIntBE(recordIndex, 8);
+          quant.values.push(value);
+          recordIndex += 8;
+          break;
+        }
+        case "ULongLong": {
+          let value =
+            ergHeader.byteOrder == 0
+              ? recordBuffer.readUIntLE(recordIndex, 8)
+              : recordBuffer.readUIntBE(recordIndex, 8);
+          quant.values.push(value);
+          recordIndex += 8;
+          break;
+        }
+        case "Long": {
+          let value =
+            ergHeader.byteOrder == 0
+              ? recordBuffer.readIntLE(recordIndex, 4)
+              : recordBuffer.readIntBE(recordIndex, 4);
+          quant.values.push(value);
+          recordIndex += 4;
+          break;
+        }
+        case "ULong": {
+          let value =
+            ergHeader.byteOrder == 0
+              ? recordBuffer.readUIntLE(recordIndex, 4)
+              : recordBuffer.readUIntBE(recordIndex, 4);
+          quant.values.push(value);
+          recordIndex += 4;
+          break;
+        }
+        case "Int": {
+          let value =
+            ergHeader.byteOrder == 0
+              ? recordBuffer.readIntLE(recordIndex, 4)
+              : recordBuffer.readIntBE(recordIndex, 4);
+          quant.values.push(value);
+          recordIndex += 4;
+          break;
+        }
+        case "UInt": {
+          let value =
+            ergHeader.byteOrder == 0
+              ? recordBuffer.readUIntLE(recordIndex, 4)
+              : recordBuffer.readUIntBE(recordIndex, 4);
+          quant.values.push(value);
+          recordIndex += 4;
+          break;
+        }
+        case "Short": {
+          let value =
+            ergHeader.byteOrder == 0
+              ? recordBuffer.readIntLE(recordIndex, 2)
+              : recordBuffer.readIntBE(recordIndex, 2);
+          quant.values.push(value);
+          recordIndex += 4;
+          break;
+        }
+        case "UShort": {
+          let value =
+            ergHeader.byteOrder == 0
+              ? recordBuffer.readUIntLE(recordIndex, 2)
+              : recordBuffer.readUIntBE(recordIndex, 2);
+          quant.values.push(value);
+          recordIndex += 4;
+          break;
+        }
+        case "Char": {
+          let value =
+            ergHeader.byteOrder == 0
+              ? recordBuffer.readIntLE(recordIndex, 1)
+              : recordBuffer.readIntBE(recordIndex, 1);
+          quant.values.push(value.toString());
+          recordIndex += 1;
+          break;
+        }
+        case "UChar": {
+          let value =
+            ergHeader.byteOrder == 0
+              ? recordBuffer.readUIntLE(recordIndex, 1)
+              : recordBuffer.readUIntBE(recordIndex, 1);
+          quant.values.push(value.toString());
+          recordIndex += 1;
+          break;
+        }
+        default: {
+          if (quant.type.endsWith("Bytes")) {
+            let skipBytes = parseInt(quant.type.split(" Bytes")[0]);
+            recordIndex += skipBytes;
+          }
+          break;
+        }
+      }
+    });
+  }
+  return quants;
+};
